2cceaf196e94ed9d1e2a72cc66ee69682e4c2a06
yashodhi Chathurangi <yashodhichathurangi94@gmail.com>
2023-06-13 10:31:10 +0300

    add store and cart items, install redux tool

---
+++ starter/src/App.js
@@ -0,0 +1,9 @@
+import CartContainer from "./components/CartContainer";
+import Navbar from "./components/Navbar";
+function App() {
+  return <main>
+    <Navbar />
+    <CartContainer />
+  </main>;
+}
+export default App;
---
+++ starter/src/features/cart/cartSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from '@reduxjs/toolkit';
+import cartItems from '../../cartItems';
+const initialState = {
+  cartItems: cartItems,
+  amount: 4,
+  total: 0,
+  isLoading: true,
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+});
+
+//console.log(cartSlice);
+
+export default cartSlice.reducer;
